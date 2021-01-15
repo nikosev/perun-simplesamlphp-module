58db9b0d345ea94ce1734dda355edce3006e8fd6
nikosev <nikos.ev@hotmail.com>
2021-01-15 18:34:59 +0200

    Add new attributemap

---
+++ attributemap/perunoid2oid.php
@@ -0,0 +1,7 @@
+<?php
+$attributemap = array(
+
+    // Attributes returned by Perun IdPs, e.g. EGI SSO
+    // 'userCertificateSubject'=> 'distinguishedName',
+    'urn:oid:1.3.6.1.4.1.11433.2.2.1.9'  => 'urn:oid:2.5.4.49',
+);
